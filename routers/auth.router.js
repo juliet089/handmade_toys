62139e60df70155c73b9119bbc0f35a38a61dcaa
juliet089 <lenamk019@yandex.ru>
2025-06-21 17:13:40 +0500

    Delete routers directory

--- routers/auth.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require("../controllers/auth.controller");
-const authRouter = express.Router();
-
-/*обработка маршрутов*/
-authRouter.post("/login", authController.login);
-authRouter.post("/registration", authController.registration);
-
-module.exports = authRouter;
