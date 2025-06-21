e0a50fed99ddff87dae341d8a9ac11698f19d321
juliet089 <lenamk019@yandex.ru>
2025-06-21 17:56:23 +0500

    Add files via upload

---
+++ frontend/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import {vitePreprocess} from "@sveltejs/kit/vite";
+
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
+    kit: {
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        appDir: 'app',
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH
+        }
+    },
+    preprocess: vitePreprocess()
+};
+
+export default config;
