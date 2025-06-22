48ad16afbc0c07c84f85993f534b613ebef4e854
juliet089 <lenamk019@yandex.ru>
2025-06-22 15:09:12 +0500

    Delete backend directory

--- backend/models/res.Obj.class.js
+++
@@ -1,9 +0,0 @@
-class ResObj {
-    constructor(obj) {
-        this.text = obj.text ?? 'SUCCESS';
-        this.status = obj.status ?? 200;
-        this.data = obj.data ?? null;
-    }
-}
-
-module.exports = ResObj;
